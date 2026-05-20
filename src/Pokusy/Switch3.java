1dcd3c8618f6f0a12674306ba7f734dfd3f79f4e
Marek Hýža <marekhyza@icloud.com>
2026-05-20 18:41:38 +0200

    Swith3 dny v týdnu

--- src/Pokusy/Switch3.java
+++ src/Pokusy/Switch3.java
@@ -1,12 +1,37 @@
 package Pokusy;
 
+import java.util.Scanner;
+
 public class Switch3 {
     public static void main(String[] args) {
-        int den;
-        switch (den){
-            case pondeli, utery:
-                System.out.println("Dny v tydnu");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("zadej číslo dne v týdnu");
+        int den = sc.nextInt();
+        switch (den) {
+            case 1:
+                System.out.println("Pondělí");
                 break;
+
+            case 2:
+                System.out.println("úterý");
+                break;
+            case 3:
+                System.out.println("středa");
+                break;
+            case 4:
+                System.out.println("čtvrtek");
+                break;
+            case 5:
+                System.out.println("pátek");
+                break;
+            case 6:
+                System.out.println("sobota");
+                break;
+            case 7:
+                System.out.println("neděle");
+                break;
+
         }
     }
 }
