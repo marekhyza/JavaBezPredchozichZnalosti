1aee1dfb3e8d29783c3c96e594e3b59d2797a4e2
Marek Hýža <marekhyza@icloud.com>
2026-05-20 18:54:21 +0200

    Swith3 dny v týdnu - přidáno heslo

--- src/Pokusy/Switch3.java
+++ src/Pokusy/Switch3.java
@@ -4,34 +4,40 @@ import java.util.Scanner;
 
 public class Switch3 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.println("zadej číslo dne v týdnu");
-        int den = sc.nextInt();
-        switch (den) {
-            case 1:
-                System.out.println("Pondělí");
-                break;
+        Scanner sc2 = new Scanner(System.in);
+        System.out.println("Zadej heslo pro spuštění programu! Špatně zadané heslo program ukončí!");
+        int heslo = sc2.nextInt();
+        if (heslo == 1111) {
 
-            case 2:
-                System.out.println("úterý");
-                break;
-            case 3:
-                System.out.println("středa");
-                break;
-            case 4:
-                System.out.println("čtvrtek");
-                break;
-            case 5:
-                System.out.println("pátek");
-                break;
-            case 6:
-                System.out.println("sobota");
-                break;
-            case 7:
-                System.out.println("neděle");
-                break;
+            Scanner sc = new Scanner(System.in);
+            System.out.println("zadej číslo dne v týdnu");
+            int den = sc.nextInt();
+            switch (den) {
+                case 1:
+                    System.out.println("Pondělí");
+                    break;
 
+                case 2:
+                    System.out.println("úterý");
+                    break;
+                case 3:
+                    System.out.println("středa");
+                    break;
+                case 4:
+                    System.out.println("čtvrtek");
+                    break;
+                case 5:
+                    System.out.println("pátek");
+                    break;
+                case 6:
+                    System.out.println("sobota");
+                    break;
+                case 7:
+                    System.out.println("neděle");
+                    break;
+
+            }
         }
     }
 }
